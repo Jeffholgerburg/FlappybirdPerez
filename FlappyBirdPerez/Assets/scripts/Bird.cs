2a1d46c361252657fd3cc8532bc2c3db2f8ff51b
Fabian Perez <145504952+Jeffholgerburg@users.noreply.github.com>
2023-12-06 21:43:35 -0800

    What I did
    I tried to make the bird move but didn't and made a script for the bird.

--- FlappyBirdPerez/Assets/scripts/Bird.cs
+++ FlappyBirdPerez/Assets/scripts/Bird.cs
@@ -4,20 +4,31 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
+    public float upForce = 0f;
+
     private bool isDead = false;
+    private Rigidbody2D rb2d;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isDead) == false) 
+        if (isDead == false) 
         {
-
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+            }
         }
 
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isDead = true;
     }
 }
