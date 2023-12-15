b6f32b967194db505854754e69a190420c4ec2a9
Fabian Perez <145504952+Jeffholgerburg@users.noreply.github.com>
2023-12-14 22:48:22 -0800

    What I did
    I finished the flappy bird videos.

--- FlappyBirdPerez/Assets/scripts/Columuns Pool.cs
+++ FlappyBirdPerez/Assets/scripts/Columuns Pool.cs
@@ -7,11 +7,14 @@ public class ColumunsPool : MonoBehaviour
     public int ColumunsPoolSize = 5;
     public GameObject columunsPrefab;
     public float spawnRate = 4f;
+    public float columnMin = -1f;
+    public float columnMax = 3.5f;
     
     private GameObject[] columuns;
     private Vector2 objectPoolPosition = new Vector2 (-15, -25f);
     private float timesSinceLastSpawned;
-
+    private float spawnXPosition = 10f;
+    private int currentColumn = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -27,5 +30,17 @@ public class ColumunsPool : MonoBehaviour
     void Update()
     {
         timesSinceLastSpawned += Time.deltaTime;
+
+        if (GameControl.instance.GameOver == false && timesSinceLastSpawned < spawnRate)
+        {
+            timesSinceLastSpawned = 0;
+            float spawnYPosition = Random.Range (columnMin, columnMax);
+            columuns [currentColumn].transform.position = new Vector2 (spawnXPosition, spawnYPosition);
+            currentColumn++;
+            if (currentColumn >= ColumunsPoolSize)
+            {
+                currentColumn = 0;
+            }
+        }
     }
 }
